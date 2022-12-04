130d6197b1b6dc8b2498332a41ccca4595f5d161
haselby <jeffrey.haselby@gmail.com>
2022-12-03 23:20:14 -0700

    InputReader.java class added functionality to read input from /src/main/resources

--- day1-part1/src/main/java/com/haselby/aoc2022/day1/part1/InputReader.java
+++ day1-part1/src/main/java/com/haselby/aoc2022/day1/part1/InputReader.java
@@ -1,25 +1,48 @@
 package com.haselby.aoc2022.day1.part1;
 
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 import java.io.FileReader;
+import java.net.URL;
 import java.util.List;
 
+/*
+ import com.opencsv.exceptions.CsvValidationException;
+ import java.io.FileNotFoundException;
+ import java.io.IOException;
+ import java.util.Arrays;
+*/
+
 // Attribution https://www.geeksforgeeks.org/reading-csv-file-java-using-opencsv/
+// Attribution: https://howtodoinjava.com/java/library/parse-read-write-csv-opencsv/
 public class InputReader {
 
     // Java code to illustrate reading all data at once
     public static void readAllDataAtOnce(String file) {
         try {
-            // Create an object of file reader
-            // class with CSV file as a parameter.
-            FileReader filereader = new FileReader(file);
 
-            // create csvReader object and skip first Line
-            com.opencsv.CSVReader csvReader = new CSVReaderBuilder(filereader).withSkipLines(1).build();
+            // Create an object of file reader capable of reading file from /scr/main/resources/
+            URL fileUrl = InputReader.class.getClassLoader().getResource(file);
+            FileReader filereader = new FileReader(fileUrl.getFile());
+
+            // Create csvReader object
+            // Default separator is comma
+            // Start reading from line number 0 (line numbers start from zero)
+            CSVParser csvParser = new CSVParserBuilder()
+                    .withSeparator(',')
+                    .withIgnoreQuotations(true)
+                    .build();
+
+            CSVReader csvReader = new CSVReaderBuilder(filereader)
+                    .withSkipLines(0)
+                    .withCSVParser(csvParser)
+                    .build();
 
             List<String[]> allData = csvReader.readAll();
 
-            // print Data
+            // Print Data
             for (String[] row : allData) {
                 for (String cell : row) {
                     System.out.print(cell + "\t");
--- day1-part1/src/main/java/com/haselby/aoc2022/day1/part1/Puzzle.java
+++ day1-part1/src/main/java/com/haselby/aoc2022/day1/part1/Puzzle.java
@@ -1,14 +1,11 @@
 package com.haselby.aoc2022.day1.part1;
 
 public class Puzzle {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Hello Day1, Part1");
 
-        //Try reading puzzle input located in folder: C:\Temp\20221202\input.txt"
-        //InputReader.readAllDataAtOnce("C:\\Temp\\20221202\\input.txt");
-
-        //Try reading puzzle input located in folder: C:\Temp\20221202\input.txt"
-        ParseCSVLineByLine.goAhead();
+        //Read puzzle input located in folder: /scr/main/resources
+        InputReader.readAllDataAtOnce("input.txt");
 
     }
 }
