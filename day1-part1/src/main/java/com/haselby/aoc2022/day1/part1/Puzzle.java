31d86700c12e4ce1905654df26f3aa64f4581c99
haselby <jeffrey.haselby@gmail.com>
2022-12-08 13:28:56 -0700

    Created PuzzleInput class which provides a simple method to read in puzzle input.

--- day1-part1/src/main/java/com/haselby/aoc2022/day1/part1/Puzzle.java
+++ day1-part1/src/main/java/com/haselby/aoc2022/day1/part1/Puzzle.java
@@ -1,21 +1,27 @@
 package com.haselby.aoc2022.day1.part1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Puzzle {
     public static void main(String[] args) {
         System.out.println("Hello Day1, Part1");
 
+        List<Integer> puzzleInput = new ArrayList<>();
+
         //Read puzzle input located in folder: /scr/main/resources
         try {
-            InputFileReader.print("input.txt");
-            InputFileReader.readIntoListOfStrings("input.txt");
-            InputFileReader.readIntoListOfIntegers("input.txt");
-        }
-        catch(Exception e) {
+            puzzleInput = PuzzleInput.readIntoListOfIntegers("input.txt");
+            System.out.println("Puzzle input read successfully");
+        } catch (Exception e) {
             System.out.println("Puzzle input is missing. Closing application");
+            e.printStackTrace();
             System.exit(0);
         }
 
-        System.out.println("Puzzle input read successfully");
+        for (Integer x : puzzleInput) {
+            System.out.println(x);
+        }
 
     }
 }
---
+++ day1-part1/src/main/java/com/haselby/aoc2022/day1/part1/PuzzleInput.java
@@ -0,0 +1,36 @@
+package com.haselby.aoc2022.day1.part1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PuzzleInput {
+
+    public static List<Integer> readIntoListOfIntegers(String filename) throws IOException {
+
+        List<Integer> listOfIntegers = new ArrayList<>();
+
+        // Find input file in resources folder: /scr/main/resources
+        URL puzzleInputFileUrl = InputFileReader.class.getClassLoader().getResource(filename);
+        var file = (puzzleInputFileUrl != null) ? puzzleInputFileUrl.getFile() : null;
+        if (file == null) throw new IOException("Input filename does not exist!");
+
+        try (BufferedReader puzzleInputFile = new BufferedReader(new FileReader(file))) {
+            String input;
+            while ((input = puzzleInputFile.readLine()) != null) {
+                String[] data = input.split(",");
+                //Insert null into List of Integers where input file contains empty line
+                Integer x = (data[0].isEmpty()) ? null : Integer.parseInt(data[0]);
+                listOfIntegers.add(x);
+            }
+
+        } catch (IOException e) {
+            throw new IOException();
+        }
+
+        return listOfIntegers;
+    }
+}
