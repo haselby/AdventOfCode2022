0c87698b8a42b958d4774760de8e8b2a14b1f1c4
haselby <jeffrey.haselby@gmail.com>
2022-12-08 23:46:30 -0700

    D2P1: Updated PuzzleInput and PuzzleInputTest to read into 2D ArrayList of Characters

--- day2-part1/src/main/java/com/haselby/aoc2022/day2/part1/PuzzleInput.java
+++ day2-part1/src/main/java/com/haselby/aoc2022/day2/part1/PuzzleInput.java
@@ -8,9 +8,9 @@ import java.util.ArrayList;
 
 public class PuzzleInput {
 
-    public static ArrayList<Character> readIntoListOfCharacters(String filename) throws IOException {
+    public static ArrayList<ArrayList<Character>> readIntoListOfCharacters(String filename) throws IOException {
 
-        ArrayList<Character> listOfCharacters = new ArrayList<>();
+        ArrayList<ArrayList<Character>> listOfCharacters = new ArrayList<>();
 
         // Find input file in resources folder: /scr/main/resources
         URL puzzleInputFileUrl = PuzzleInput.class.getClassLoader().getResource(filename);
@@ -18,12 +18,18 @@ public class PuzzleInput {
         if (file == null) throw new IOException("Input filename does not exist!");
 
         try (BufferedReader puzzleInputFile = new BufferedReader(new FileReader(file))) {
+            int index = 0;
             String input;
             while ((input = puzzleInputFile.readLine()) != null) {
                 String[] data = input.split(" ");
                 //Insert null into List of Integers where input file contains empty line
                 Character x = (data[0].isEmpty()) ? null : data[0].charAt(0);
-                listOfCharacters.add(x);
+                Character y = (data[1].isEmpty()) ? null : data[1].charAt(0);
+                //Allocates space in 2D ArrayList of Characters [[A,B], [J,H] ....]
+                listOfCharacters.add(new ArrayList<>());
+                listOfCharacters.get(index).add(0, x);
+                listOfCharacters.get(index).add(1, y);
+                index++;
             }
 
         } catch (IOException e) {
--- day2-part1/src/test/java/com/haselby/aoc2022/day2/part1/PuzzleInputTest.java
+++ day2-part1/src/test/java/com/haselby/aoc2022/day2/part1/PuzzleInputTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -44,9 +43,10 @@ public class PuzzleInputTest {
     }
 
     @Test
-    @DisplayName("readIntoListOfIntegers - passing example input file - returns expected list of Integers")
-    void readIntoListOfIntegers_ExampleInputFile_ReturnsExpectedListOfIntegers() {
-        List<Character> puzzleInputTest01 = new ArrayList<>();
+    @DisplayName("readInto2DListOfCharacters - passing example input file - returns expected list of Characters")
+    void readInto2DListOfCharacters_ExampleInputFile_ReturnsExpectedListOfCharacters() {
+
+        ArrayList<ArrayList<Character>> puzzleInputTest01 = new ArrayList<>();
 
         try {
             puzzleInputTest01 = PuzzleInput.readIntoListOfCharacters("test-input-d2p1-01.txt");
@@ -55,7 +55,19 @@ public class PuzzleInputTest {
             e.printStackTrace();
         }
 
-        assertEquals('A', puzzleInputTest01.get(0));
+        // Validate Te
+        //  A Y
+        //  B X
+        //  C Z
+
+        assertEquals('A', puzzleInputTest01.get(0).get(0));
+        assertEquals('Y', puzzleInputTest01.get(0).get(1));
+
+        assertEquals('B', puzzleInputTest01.get(1).get(0));
+        assertEquals('X', puzzleInputTest01.get(1).get(1));
+
+        assertEquals('C', puzzleInputTest01.get(2).get(0));
+        assertEquals('Z', puzzleInputTest01.get(2).get(1));
 
 
     }
