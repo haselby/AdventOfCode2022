6084dc0caf90b0ae074e224560a14434add3c57c
haselby <jeffrey.haselby@gmail.com>
2022-12-08 22:52:57 -0700

    Created project structure for day2-part1

--- day2-part1/src/test/java/com/haselby/aoc2022/day2/part1/PuzzleTest.java
+++ day2-part1/src/test/java/com/haselby/aoc2022/day2/part1/PuzzleTest.java
@@ -1,4 +1,11 @@
 package com.haselby.aoc2022.day2.part1;
+import org.junit.jupiter.api.*;
 
+@DisplayName("Day 2, Part 1 - Puzzle Test")
 public class PuzzleTest {
+    @Test
+    @DisplayName("First test")
+    void firstTest() {
+        System.out.println("First test method");
+    }
 }
