1c7979d2559474e9b490817b0e7c9957d6a5dbe9
haselby <jeffrey.haselby@gmail.com>
2022-12-08 23:54:58 -0700

    D2P1: Updated Puzzle Class to read in input.txt and print out 2D ArrayList of Characters

--- day2-part1/src/main/java/com/haselby/aoc2022/day2/part1/Puzzle.java
+++ day2-part1/src/main/java/com/haselby/aoc2022/day2/part1/Puzzle.java
@@ -1,8 +1,30 @@
 package com.haselby.aoc2022.day2.part1;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Puzzle {
 
     public static void main(String[] args) {
+        System.out.println("Hello Day2, Part1");
+
+        ArrayList<ArrayList<Character>> puzzleInput = new ArrayList<>();
+
+        //Read puzzle input located in folder: /scr/main/resources
+        try {
+            puzzleInput = PuzzleInput.readIntoListOfCharacters("input.txt");
+            System.out.println("Puzzle input read successfully");
+        } catch (IOException e) {
+            System.out.println("Puzzle input is missing. Closing application");
+            e.printStackTrace();
+            System.exit(0);
+        }
+
+        for (ArrayList<Character> xy : puzzleInput ){
+            System.out.println(xy);
+        }
+
+
 
     }
 }
